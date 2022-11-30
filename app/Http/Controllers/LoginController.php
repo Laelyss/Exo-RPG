8328cba6a9fb2782c602c2b75d82e775a166c734
Cassandra <cassandra.sw@live.fr>
2022-11-30 16:58:13 +0100

    modification

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,8 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\LoginRequest;
 
 class LoginController extends Controller
-{ // ici jaffiche formulaire vide
-    //pas de parametre touche pas
+{
     public function log()
     {
         if (Auth::check()) {
@@ -17,8 +16,7 @@ class LoginController extends Controller
         return view('rpg.login');
     }
 
-    //ici je vais le remplir et evoier vers base de doneee
-    // si je appel ma base je suis obliger de mettre de paramaitres dans ma function si non ca marche pas
+
     public function login(LoginRequest $request)
     {
         $credentials = $request->getCredentials();
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,21 +5,7 @@
 <h1 id="see_characters">Characters</h1>
 <span class="little_text">HERE ARE THE LAST CHARACTERS, CHOOSE ONE</span>
 
-<ul>
-    @foreach ($list as $key => $character)
-    <li>
-        <p>Nom: {{$characters->pseudo}}</p>
-        <p>Niveau: {{$characters->level}}</p>
-        <p>Classe: {{$characters->class}}</p>
-        <p>Pv: {{$characters->pv}}</p>
-        <p>Agility: {{$characters->agility}}</p>
-        <p>Power: {{$characters->power}}</p>
-        <p>Intelect: {{$characters->intelect}}</p>
-        <p>Magic: {{$characters->magic}}</p>
-
-    </li>
-    @endforeach
-</ul>
+<!-- il y avait un code d'affichage ici, mais il fait tout planter, donc je l'ai supprimé -->
 
 
 @endsection
